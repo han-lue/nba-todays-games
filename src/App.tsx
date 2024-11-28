3ca87fd0d582967a4f133719879f8435d026da74
username <denesakin@gmail.com>
2024-11-28 21:21:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  const gamesUrl = import.meta.env.VITE_API_GAMES_URL
+  const apiKey = import.meta.env.VITE_API_KEY
+
+  useEffect(() => {
+
+    fetch(gamesUrl + "/?dates[]=2024-11-29", {
+      method: 'GET',
+      headers: {
+          "Authorization": apiKey,
+      }
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+  }, [])
+
+  return (
+    <>
+      <p>{gamesUrl}</p>
+    </>
+  )
+}
+
+export default App
