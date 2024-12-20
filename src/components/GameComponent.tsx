f65904f1eca94c269a0a5ef83f2a38209cbb59c1
username <denesakin@gmail.com>
2024-12-20 14:14:02 +0100

    code refactor and icon change

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -1,86 +1,106 @@
-import { GameType } from '../types/Game.type'
+import { GameType } from "../types/Game.type";
 
-import '../styles/Game.css'
+import "../styles/Game.css";
 
 interface Props {
-    game: GameType,
+	game: GameType;
 }
 
-const Game:React.FC<Props> = ({game}) => {
+const Game: React.FC<Props> = ({ game }) => {
+	let fullDate: string = "";
+	let localTime: string = "";
 
-  let fullDate:string = "";
-  let localTime:string = "";
+	let hasStarted: boolean;
 
-  let hasStarted: boolean;
+	// Check if the game has started and set either current state or start date & time
+	if (
+		game.time === "1st Qtr" ||
+		game.time === "2nd Qtr" ||
+		game.time === "Halftime" ||
+		game.time === "3rd Qtr" ||
+		game.time === "4th Qtr" ||
+		game.time === "Final"
+	) {
+		hasStarted = true;
+		localTime = game.time;
+	} else {
+		hasStarted = false;
+		const gameDate = new Date(game.time);
 
-  // Check if the game has started and set either current state or start date & time
-  if (game.time === "1st Qtr" ||
-    game.time === "2nd Qtr" ||
-    game.time === "Halftime" ||
-    game.time === "3rd Qtr" ||
-    game.time === "4th Qtr" ||
-    game.time === "Final") {
-        
-    hasStarted = true;
-    localTime = game.time;
-  
-  } else {
-    hasStarted = false;
-    const gameDate = new Date(game.time); 
+		// Format game's date
+		fullDate =
+			("0" + gameDate.getDate().toString()).slice(-2) +
+			"-" +
+			("0" + (gameDate.getMonth() + 1).toString()).slice(-2) +
+			"-" +
+			gameDate.getFullYear().toString();
 
-    // Format game's date
-    fullDate = ("0" + gameDate.getDate().toString()).slice(-2) + "-" + 
-    ("0" + (gameDate.getMonth() + 1).toString()).slice(-2) + "-" + 
-    gameDate.getFullYear().toString()
+		// Format game's time
+		localTime =
+			("0" + gameDate.getHours().toString()).slice(-2) +
+			":" +
+			("0" + gameDate.getMinutes().toString()).slice(-2);
+	}
 
-    // Format game's time
-    localTime = ("0" + gameDate.getHours().toString()).slice(-2) + ":" + 
-    ("0" + gameDate.getMinutes().toString()).slice(-2)
-  }
-  
+	return (
+		<div className="game">
+			<div className="game__team">
+				<p>{game.homeTeam.name}</p>
+				<div className="game__team__image">
+					<img
+						src={`/assets/logos/${game.homeTeam.abbreviation}.svg`}
+						alt={`NBA team ${game.homeTeam.name}'s logo`}
+					/>
+				</div>
+			</div>
 
-  return (
-    <div className='game'>  
+			{hasStarted ? (
+				// Game has started, showing scores instead of time
+				<div className="game--has-started">
+					<div className="game--has-started__scores">
+						<p
+							id="homeTeamScore"
+							className={
+								Number(game.homeTeam.score) < Number(game.awayTeam.score)
+									? "game--has-started__scores--loser"
+									: ""
+							}
+						>
+							{game.homeTeam.score}
+						</p>
+						<p>:</p>
+						<p
+							id="awayTeamScore"
+							className={
+								Number(game.homeTeam.score) > Number(game.awayTeam.score)
+									? "game--has-started__scores--loser"
+									: ""
+							}
+						>
+							{game.awayTeam.score}
+						</p>
+					</div>
+					<p className="game--has-started__status">{localTime}</p>
+				</div>
+			) : (
+				// Game hasn't started, showing date and time
+				<div className="game--hasnt-started__status">
+					<p className="game--hasnt-started__status__time">{localTime}</p>
+					<p className="game--hasnt-started__status__date">{fullDate}</p>
+				</div>
+			)}
 
-      <div className='game__team'>
-        <p>{game.homeTeam.name}</p>
-        <div className='game__team__image'>
-          <img src={`/assets/logos/${game.homeTeam.abbreviation}.svg`} alt={`NBA team ${game.homeTeam.name}'s logo`}/>
-        </div>
-      </div>
+			<div className="game__team">
+				<p>{game.awayTeam.name}</p>
+				<div className="game__team__image">
+					<img
+						src={`/assets/logos/${game.awayTeam.abbreviation}.svg`}
+						alt={`NBA team ${game.awayTeam.name}'s logo`}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-      {
-      hasStarted ? 
-      ( // Game has started, showing scores instead of time
-        <div className='game--has-started'>
-          <div className='game--has-started__scores'>
-            <p id='homeTeamScore' 
-            className={(Number(game.homeTeam.score) < Number(game.awayTeam.score)) ? "game--has-started__scores--loser" : ""}>{game.homeTeam.score}</p>
-            <p>:</p>
-            <p id='awayTeamScore'
-            className={(Number(game.homeTeam.score) > Number(game.awayTeam.score)) ? "game--has-started__scores--loser" : ""}>{game.awayTeam.score}</p>
-          </div>
-          <p className='game--has-started__status'>{localTime}</p>
-        </div>
-      ) 
-      : 
-      ( // Game hasn't started, showing date and time
-        <div className='game--hasnt-started__status'>
-          <p className='game--hasnt-started__status__time'>{localTime}</p>
-          <p className='game--hasnt-started__status__date'>{fullDate}</p>
-        </div>
-      )
-      }
-
-      <div className='game__team'>
-        <p>{game.awayTeam.name}</p>
-        <div className='game__team__image'>
-          <img src={`/assets/logos/${game.awayTeam.abbreviation}.svg`} alt={`NBA team ${game.awayTeam.name}'s logo`}/>
-        </div>
-      </div>
-      
-    </div>
-  )
-}
-
-export default Game
+export default Game;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,100 +1,96 @@
-import { useEffect, useState } from 'react'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { useEffect, useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-import Game from "../components/GameComponent"
-import '../styles/HomePage.css'
+import Game from "../components/GameComponent";
+import "../styles/HomePage.css";
 
-import dayjs, { Dayjs } from 'dayjs'
+import dayjs, { Dayjs } from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat.js";
 dayjs.extend(customParseFormat);
 
 function Games() {
-  // Get todays date
-  const todaysDate = dayjs(); 
+	// Get todays date
+	const todaysDate = dayjs();
 
-  const [games, setGames] = useState<Array<any> | null>(null);
-  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(todaysDate);
+	const [games, setGames] = useState<Array<any> | null>(null);
+	const [selectedDate, setSelectedDate] = useState<Dayjs | null>(todaysDate);
 
-  // Get API key and URL from .env file
-  const apiGamesUrl = import.meta.env.VITE_API_GAMES_URL;
-  const apiKey = import.meta.env.VITE_API_KEY;
+	// Get API key and URL from .env file
+	const apiGamesUrl = import.meta.env.VITE_API_GAMES_URL;
+	const apiKey = import.meta.env.VITE_API_KEY;
 
-  // Fetch games of today or the selected date
-  useEffect(() => {
-    fetch(apiGamesUrl + "/?dates[]=" + selectedDate!.format("YYYY-MM-DD"), {
-      method: 'GET',
-      headers: { "Authorization" : apiKey }
-    })
-    .then(response => response.json())
-    .then(response => setGames(response.data.slice()))
-    .catch(error => console.error(error));
-     
-  }, [selectedDate]);
+	// Fetch games of today or the selected date
+	useEffect(() => {
+		fetch(apiGamesUrl + "/?dates[]=" + selectedDate!.format("YYYY-MM-DD"), {
+			method: "GET",
+			headers: { Authorization: apiKey },
+		})
+			.then((response) => response.json())
+			.then((response) => setGames(response.data.slice()))
+			.catch((error) => console.error(error));
+	}, [selectedDate]);
 
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className='home-page'> 
-        <div className='home-page__container'>
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<div className="home-page">
+				<div className="home-page__container">
+					<div>
+						<DatePicker
+							label="Date"
+							value={selectedDate}
+							onAccept={(newDate) => setSelectedDate(newDate)} // onAccept is fired whenever the date is fully selected,
+							//in contrast to onChange, which is fired even when you change month or year.
+							views={["year", "month", "day"]}
+						/>
+					</div>
 
-          <div >
-            <DatePicker 
-            label="Date"
-            value={selectedDate}
-            onAccept={(newDate) => setSelectedDate(newDate)} // onAccept is fired whenever the date is fully selected, 
-            //in contrast to onChange, which is fired even when you change month or year.
-            views={['year', "month", "day"]}
-            />  
-          </div> 
-          
-          <div className='home-page__container__grid'>
-            { 
-            games === null 
-            ? 
-            // If waiting for API response, show loading animation
-            <Box sx={{ display: 'flex', justifySelf: "center" }}>
-              <CircularProgress />
-            </Box>
-            : 
-              games.length === 0 
-              ? 
-              // If there's no games on the selected date, show no games text
-              <p className='home-page__text--no-games'>There are no games today</p>
-              : 
-              // If there are games on the selected date, show the games
-              games.map((game: any) => 
-                <Game key={game.id}
-
-                game = {{
-                  id: game.id,
-                  postSeason: game.postseason,
-                  time: game.status,
-                  homeTeam: {
-                    id: game.home_team.id, 
-                    name: game.home_team.name, 
-                    abbreviation: game.home_team.abbreviation, 
-                    score: game.home_team_score,
-                    conference: game.home_team.conference 
-                  },
-                  awayTeam: {
-                    id: game.visitor_team.id,
-                    name: game.visitor_team.name,
-                    abbreviation: game.visitor_team.abbreviation,
-                    score: game.visitor_team_score,
-                    conference: game.visitor_team.conference,
-                  }
-                }} 
-                />
-              )
-            }
-          </div>
-        </div>
-      </div>
-    </LocalizationProvider>
-  )
+					<div className="home-page__container__grid">
+						{games === null ? (
+							// If waiting for API response, show loading animation
+							<Box sx={{ display: "flex", justifySelf: "center" }}>
+								<CircularProgress />
+							</Box>
+						) : games.length === 0 ? (
+							// If there's no games on the selected date, show no games text
+							<p className="home-page__text--no-games">
+								There are no games today
+							</p>
+						) : (
+							// If there are games on the selected date, show the games
+							games.map((game: any) => (
+								<Game
+									key={game.id}
+									game={{
+										id: game.id,
+										postSeason: game.postseason,
+										time: game.status,
+										homeTeam: {
+											id: game.home_team.id,
+											name: game.home_team.name,
+											abbreviation: game.home_team.abbreviation,
+											score: game.home_team_score,
+											conference: game.home_team.conference,
+										},
+										awayTeam: {
+											id: game.visitor_team.id,
+											name: game.visitor_team.name,
+											abbreviation: game.visitor_team.abbreviation,
+											score: game.visitor_team_score,
+											conference: game.visitor_team.conference,
+										},
+									}}
+								/>
+							))
+						)}
+					</div>
+				</div>
+			</div>
+		</LocalizationProvider>
+	);
 }
 
-export default Games
+export default Games;
